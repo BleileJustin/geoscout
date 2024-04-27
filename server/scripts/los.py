339d5994f1e86918062023d3a3f1d063370c7a46
Justin Bleile <justin.bleile@iqgeo.com>
2024-04-27 13:24:37 -0600

    Uses python to read and display tif DEM data

--- server/scripts/los.py
+++ server/scripts/los.py
@@ -1,1 +1,14 @@
-# Line of Site geotiff data processing algogrithm
+# Line of sight geotiff data processing algogrithm
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+import rasterio
+from rasterio.plot import show
+
+dem = rasterio.open('data/output_COP30.tif')
+dem_array = dem.read(1)
+transform = dem.transform
+
+plt.imshow(dem_array, cmap='gray')
+plt.colorbar()
+plt.show()
